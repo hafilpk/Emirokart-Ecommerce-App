e230e5452476810cc85f9bbab33153ad1fb23312
hafilpk <pkmshafil@gmail.com>
2025-09-09 23:36:06 +0530

    create jwt token authentication

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
